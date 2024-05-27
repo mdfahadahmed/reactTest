e13b8635537da10a752c13adcc7fe4aa5e244693
“mdfahadahmed” <fahadahmed492@gmail.com>
2024-05-27 18:37:30 +0600

    second

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Header/>
-        <div className="py-20 px-3 bg-indigo-950">
+        <div className="s py-20 px-3 bg-indigo-950">
           <h1 className="text-center font-bold text-5xl text-white pb-14 font-mono">React Project</h1>
           <div className="flex justify-center items-center gap-10">
             <Home/>
